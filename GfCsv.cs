040edb1cc16375f003e7e4824a0a7c7a9d0ee1cd
com.chinaos.snaill <com.chinaos.snaill@3b7b01ba-a116-11de-9f90-09b151b1f2f6>
2009-09-14 15:54:56 +0000

    实现招商证券格式化文本分析类,未测试;实现基础界面(仅UI);添加招商证券格式化文本样例

--- GfCsv.cs
+++ GfCsv.cs
@@ -4,6 +4,18 @@ using System.Text;
 
 namespace Jeebook._2Gf
 {
+    enum GfType
+    {
+        // 买入
+        Buy,
+        // 买出
+        Sell,
+        // 存入现金
+        DepositCash,
+        // 取出现金
+        WithdrawCash
+    }
+
     class GfRecord
     {
         // 股票代码
@@ -11,7 +23,7 @@ namespace Jeebook._2Gf
         // 名称
         public string Name;
         // 类型
-        public string Type;
+        public GfType Type;
         // 日期
         public string Date;
         // 股数
@@ -30,7 +42,7 @@ namespace Jeebook._2Gf
     {
         System.Collections.ArrayList arRecords = new System.Collections.ArrayList();
 
-        public void Add(string Symbol, string Name, string Type, string Date, string Shares, string Price, string Commission, string Notes)
+        public void Add(string Symbol, string Name, GfType Type, string Date, string Shares, string Price, string Commission, string Notes)
         {
             GfRecord rec = new GfRecord();
             rec.Symbol = Symbol;
@@ -45,7 +57,7 @@ namespace Jeebook._2Gf
 
         }
 
-        public void Add(string Type, string Date, string Value, string Notes)
+        public void Add(GfType Type, string Date, string Value, string Notes)
         {
             GfRecord rec = new GfRecord();
             rec.Name = "Cash";
@@ -68,8 +80,16 @@ namespace Jeebook._2Gf
             for (int i = 0; i < arRecords.Count; i++)
             {
                 GfRecord    rec = (GfRecord)arRecords[i];
-                sw.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8}", 
-                    rec.Symbol, rec.Name, rec.Type, rec.Date, rec.Shares, rec.Price, rec.CashValue, rec.Commission, rec.Notes);
+                string strType = "Unknown";
+                switch (rec.Type)
+                {
+                    case GfType.Buy: strType = "Buy"; break;
+                    case GfType.Sell: strType = "Sell"; break;
+                    case GfType.DepositCash: strType = "Deposit Cash"; break;
+                    case GfType.WithdrawCash: strType = "Withdraw Cash"; break;
+                }
+                sw.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8}",
+                    rec.Symbol, rec.Name, strType, rec.Date, rec.Shares, rec.Price, rec.CashValue, rec.Commission, rec.Notes);
             }
             sw.Close();
         }
