66e026c489fc87b0ad3f0cecbd269c18598c899d
com.chinaos.snaill <com.chinaos.snaill@3b7b01ba-a116-11de-9f90-09b151b1f2f6>
2009-09-14 11:36:32 +0000

    导入初实现程序和例子csv文件

---
+++ trunk/GfCsv.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jeebook._2Gf
+{
+    class GfRecord
+    {
+        // 股票代码
+        public string Symbol;
+        // 名称
+        public string Name;
+        // 类型
+        public string Type;
+        // 日期
+        public string Date;
+        // 股数
+        public string Shares;
+        // 价格
+        public string Price;
+        // 现金价值
+        public string CashValue;
+        // 佣金
+        public string Commission;
+        // 备注
+        public string Notes;
+    }
+
+    class GfCsv
+    {
+        System.Collections.ArrayList arRecords = new System.Collections.ArrayList();
+
+        public void Add(string Symbol, string Name, string Type, string Date, string Shares, string Price, string Commission, string Notes)
+        {
+            GfRecord rec = new GfRecord();
+            rec.Symbol = Symbol;
+            rec.Name = Name;
+            rec.Type = Type;
+            rec.Date = Date;
+            rec.Shares = Shares;
+            rec.Price = Price;
+            rec.Commission = Commission;
+            rec.Notes = Notes;
+            Add(rec);
+
+        }
+
+        public void Add(string Type, string Date, string Value, string Notes)
+        {
+            GfRecord rec = new GfRecord();
+            rec.Name = "Cash";
+            rec.Type = Type;
+            rec.Date = Date;
+            rec.CashValue = Value;
+            rec.Notes = Notes;
+            Add(rec);
+        }
+
+        public void Add(GfRecord rec)
+        {
+            arRecords.Add(rec);
+        }
+
+        public void ToCsv(string fn)
+        {
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(fn);
+            sw.WriteLine("Symbol,Name,Type,Date,Shares,Price,Cash value,Commission,Notes");
+            for (int i = 0; i < arRecords.Count; i++)
+            {
+                GfRecord    rec = (GfRecord)arRecords[i];
+                sw.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8}", 
+                    rec.Symbol, rec.Name, rec.Type, rec.Date, rec.Shares, rec.Price, rec.CashValue, rec.Commission, rec.Notes);
+            }
+            sw.Close();
+        }
+
+    }
+}
---
+++ trunk/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("2Gf")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sursen")]
+[assembly: AssemblyProduct("2Gf")]
+[assembly: AssemblyCopyright("Copyright © Sursen 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d086127f-2af6-444c-bcb3-6a52c3a9bbd4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
